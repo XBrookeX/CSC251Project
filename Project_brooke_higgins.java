24e030346f00afb84658e464d5375cf10b0129a1
XBrookeX <brookehiggins1230@gmail.com>
2024-06-22 22:24:39 -0400

    Update Project_brooke_higgins.java
    Modified to read input from a file

--- Project_brooke_higgins.java
+++ Project_brooke_higgins.java
@@ -1,43 +1,41 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Project_brooke_higgins {
 
-	public static void main(String[] args) {
-		//Initializing scanner
-		Scanner input = new Scanner(System.in);
-		//Getting all the information from the user to create policy.
-		System.out.println("Please enter the Policy Number: ");
-		int pNumber = input.nextInt();
-		input.nextLine();
-		System.out.println("Please enter the Provider Name: ");
-		String pName = input.nextLine();
-		System.out.println("Please enter the Policyholder's First Name: ");
-		String phFirstName = input.nextLine();
-		System.out.println("Please enter the Policyholder's Last Name: ");
-		String phLastName = input.nextLine();
-		System.out.println("Please enter the Policyholder's Age: ");
-		int phAge = input.nextInt();
-		input.nextLine();
-		System.out.println("Please enter the Policyholder's Smoking Status (smoker/non-smooker): ");
-		String phSmokingStatus = input.nextLine();
-		System.out.println("Please enter the Policyholder's Height (in inches): ");
-		double phHeight = input.nextDouble();
-		System.out.println("Please enter the Policyholder's Weight (in pounds): ");
-		double phWeight = input.nextDouble();
-		//Initializing policy with user inputs
-		Policy user = new Policy(pNumber, pName, phFirstName, phLastName, phAge, phSmokingStatus, phHeight, phWeight);
-		//Displaying the results of the user's policy
-		System.out.println("Policy Number: " + user.getPolicyNumber());
-		System.out.println("Provider Name: " + user.getProviderName());
-		System.out.println("Policyholder's First Name: " + user.getPolicyholderFirstName());
-		System.out.println("Policyholder's Last Name: " + user.getPolicyholderLastName());
-		System.out.println("Policyholder's Age: " + user.getPolicyholderAge());
-		System.out.println("Policyholder's Smoking Status: " + user.getPolicyholderSmokingStatus());
-		System.out.println("Policyholder's Height: " + user.getPolicyholderHeight() + " inches");
-		System.out.println("Policyholder's Weight: " + user.getPolicyholderWeight() + " pounds");
-		System.out.println("Policyholder's BMI: " + String.format("%.2f", user.findBMI()));
-		System.out.println("Policy Price: $" + String.format("%.2f", user.calculateInsurancePolicyPrice()));
+	public static void main(String[] args) throws IOException {
 		
+		//Initializing ArrayList, Scanner, and File
+		ArrayList<Policy> policies = new ArrayList<Policy>();
+		File file = new File("PolicyInformation.txt");
+		Scanner input = new Scanner(file);
+		
+		//Loop to read through the text file and create policies until the end
+		while (input.hasNext()) {
+			//Storing all the file information into variables for the policy object values
+			String pNumber = input.nextLine();
+			String pName = input.nextLine();
+			String phFirstName = input.nextLine();
+			String phLastName = input.nextLine();
+			int phAge = input.nextInt();
+			input.nextLine();
+			String phSmokingStatus = input.nextLine();
+			double phHeight = input.nextDouble();
+			double phWeight = input.nextDouble();
+			
+			//Checking if we've reached the end of the file.
+			if (input.hasNext()) {
+				input.nextLine();
+				if (input.hasNext()) {
+					input.nextLine();
+				}
+			}
+			
+			//Using the information received and creating a Policy object
+			policies.add(new Policy(pNumber, pName, phFirstName, phLastName, phAge, phSmokingStatus, phHeight, phWeight));
+		}
 	}
 
 }
