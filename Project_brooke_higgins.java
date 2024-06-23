03c3ca5ed0ce755f668f2da358cc09b6dc46d51c
XBrookeX <brookehiggins1230@gmail.com>
2024-06-22 22:43:37 -0400

    Update Project_brooke_higgins.java
    Added code to display all policy information the number of smokers, and the number of non-smokers.

--- Project_brooke_higgins.java
+++ Project_brooke_higgins.java
@@ -35,6 +35,32 @@ public class Project_brooke_higgins {
 			
 			//Using the information received and creating a Policy object
 			policies.add(new Policy(pNumber, pName, phFirstName, phLastName, phAge, phSmokingStatus, phHeight, phWeight));
+         
+         //Variable to keep track of the number of smokers
+         int smokers = 0;
+         
+         //Loop to display information for each policy
+			for (Policy policy : policies) {
+				System.out.println("Policy Number: " + policy.getPolicyNumber());
+				System.out.println("Provider Name: " + policy.getProviderName());
+				System.out.println("Policyholder's First Name: " + policy.getPolicyholderFirstName());
+				System.out.println("Policyholder's Last Name: " + policy.getPolicyholderLastName());
+				System.out.println("Policyholder's Age: " + policy.getPolicyholderAge());
+				System.out.println("Policyholder's Smoking Status (smoker/non-smoker): " + policy.getPolicyholderSmokingStatus());
+				System.out.println("Policyholder's Height: " + policy.getPolicyholderHeight() + " inches");
+				System.out.println("Policyholder's Weight: " + policy.getPolicyholderWeight() + " pounds");
+				System.out.println("Policyholder's BMI: " + String.format("%.2f", policy.findBMI()));
+				System.out.println("Policy Price: $" + String.format("%.2f", policy.calculateInsurancePolicyPrice()));
+            
+            //If statement to determine if the current policy has a smoker
+            if (policy.getPolicyholderSmokingStatus().equalsIgnoreCase("Smoker")) {
+               smokers++;
+            }
+			}
+         
+         //Printing out
+         System.out.println("\nThe number of policies with a smoker is: " + smokers);
+         System.out.println("The number of policies with a non-smoker is: " + (policies.size() - smokers));
 		}
 	}
 
